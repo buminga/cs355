e3ecc20bfb9a05db8f456835bdc9bdbcc6d563e1
Brian Uminga <Brian.Uminga@gmail.com>
2020-07-23 23:30:01 -0400

    v5: Scrapped divs for labels. Img box clickable with CSS checkbox. Swap function able to reset checkboxes after button click

--- midterm/jquery_script.js
+++ midterm/jquery_script.js
@@ -1,75 +1,81 @@
+/***** ---------------------------
+    ** Brian Uminga
+    ** CUNY ID: 23190424
+    ** CS 355 - 17 (10:00 - 11:34)
+    ** Midterm Part B
+    ** https://buminga.github.io/cs355/midterm/
+    **
+    ** jquery_script.js
+    ** ---------------------------
+*****/
 
-        $("document").ready(function() {
-            // load images
-            var images = [];
-            var imageDir = "https://buminga.github.io/cs355/midterm/img/";
-            var imageExt = ".png"
-            for (var i = 1; i < 10; i++) {
-                images.push(imageDir + i + imageExt);
-            }
+$("document").ready(function() {
 
-            // scramble images
-            $('.container div:not(.row)').each(function() {
-                var rand = Math.floor(Math.random() * images.length);
-                $(this).append('<img class="imgBox" src="' + images[rand] + '"/>');
-                images.splice(rand, 1);
-            });
+    // load image links into array
+    var images = [];
+    var imageDir = "https://buminga.github.io/cs355/midterm/img/";
+    var imageExt = ".png"
+    for (var i = 1; i < 10; i++) {
+        images.push(imageDir + i + imageExt);
+    }
 
+    // scramble image order & insert into divs
+    var usedImg = [];
+    $('label').each(function() {
+        var rand = Math.floor(Math.random() * images.length);
+        $(this).append('<img class="imgBox" src="' + images[rand] + '"/>');
+        usedImg.push(images[rand]);
+        images.splice(rand, 1);
 
-            // assign checkbox to respective img #
-            var numVal = "";
-            var dCt = 1;
-            $('img.imgBox').each(function() {
-                numVal = $(this).prop('src'); // get # from img file
-                numVal = numVal.slice(44, 45); // slice to get just the char of img # 
-                $('#d' + dCt).prepend('<input type="checkbox" class="check" value="' + numVal + '"/>'); // add before img tag so checkbox is in front of img 
+    });
 
-                //reset values
-                numVal = "";
-                dCt += 1;
-            });
+    // assign checkbox to respective img #
+    var numVal = "";
+    var dCt = 1;
+    var aTemp = 'label[for="cb';
+    $('img.imgBox').each(function() {
+        numVal = $(this).prop('src'); // get # from img file
+        numVal = numVal.slice(44, 45); // slice to get just the char of img #
+        console.log(aTemp + dCt);
+        $(aTemp + dCt).before('<input type="checkbox" class="check" value="' + numVal + '"id=cb'+ dCt +' />'); // add before img tag so checkbox is in front of img 
+        //reset values
+        numVal = "";
+        dCt += 1;
+    });
 
-            // only allow 2 checkboxes at a time
-            var max = 2;
-            $('.check').on('change', function() {
-                if ($('.check:checked').length > max) {
-                    $(this).prop('checked', false);
-                    alert("You can only select 2 images at a time.");
-                } else {
-                    $(this).css({
-                        'background': 'blue'
-                    });
-                }
-            });
+    // only allow 2 checkboxes at a time
+    var max = 2;
+    var bTemp = 'label[for="';
+    $('.check').on('change', function() {
+        if ($('.check:checked').length > max) {
+            $(this).prop('checked', false);
+            alert("You can only select 2 images at a time.");
+        }
+    });
 
-            $("button").click(function() {
-                var favorite = [];
-                $.each($(".check:checked"), function() {
-                    favorite.push($(this).val());
-                });
-                alert("My favourite sports are: " + favorite.join(", "));
-            });
-
+    // get info from what boxes are checked
+    var selected = [];
+    $("button").click(function() {
+        $.each($(".check:checked"), function() {
+            selected.push($(this).val());
         });
 
-        $(function() {
-            $('.check').on('change', function() {
-                if ($(this).is(':checked')) {
-                    $('.divImg').fadeTo('slow', 0.3, function() {
-                        $(this).css({
-                            'background': 'blue'
-                        });
-                    }).fadeTo('slow', 1);
-                } else $(".divImg").css({
-                    'background': ''
-                });
-            });
+        console.log(selected + "yerrr");
+        var firstCheck = imageDir + selected[0] + imageExt;
+        var secondCheck = imageDir + selected[1] + imageExt;
+    });
 
-        });
 
-        var swapCounter = 0;
+});
 
-        function swapCount() {
-            swapCounter += 1;
-            document.getElementById("swapCounter").innerHTML = swapCounter;
-        };
+var swapCounter = 0;
+
+function swap() {
+    swapCounter += 1;
+    document.getElementById("swapCounter").innerHTML = swapCounter;
+    $.each($(".check:checked"), function() {
+        $(this).prop('checked', false);
+    });
+
+
+};
